847b14655d517b7c73555b95ed04ba004eacfdde
eirikhanasand <eirik.hanasand@gmail.com>
2026-04-29 06:13:15 +0200

    Bound Trivy vulnerability scans

--- src/utils/vulnerabilities/runTrivyScanRaw.ts
+++ src/utils/vulnerabilities/runTrivyScanRaw.ts
@@ -1,19 +1,27 @@
-import { exec } from 'child_process'
-import { promisify } from 'util'
-import shellEscape from './shellEscape.ts'
+import runProcessText from './runProcessText.ts'
 
-const execAsync = promisify(exec)
+const TRIVY_TIMEOUT_SECONDS = 180
 
 export default async function runTrivyScanRaw(image: string) {
-    const command = [
-        'docker run --rm',
-        '-v /var/run/docker.sock:/var/run/docker.sock',
-        '-v trivy-cache:/root/.cache/',
+    const { stdout } = await runProcessText([
+        'docker',
+        'run',
+        '--rm',
+        '-v',
+        '/var/run/docker.sock:/var/run/docker.sock',
+        '-v',
+        'trivy-cache:/root/.cache/',
         'aquasec/trivy:0.63.0',
-        'image --format json --quiet --scanners vuln',
-        shellEscape(image),
-    ].join(' ')
-
-    const { stdout } = await execAsync(command, { maxBuffer: 20 * 1024 * 1024 })
+        'image',
+        '--format',
+        'json',
+        '--quiet',
+        '--scanners',
+        'vuln',
+        image,
+    ], {
+        maxBuffer: 20 * 1024 * 1024,
+        timeoutMs: TRIVY_TIMEOUT_SECONDS * 1000,
+    })
     return JSON.parse(String(stdout))
 }
