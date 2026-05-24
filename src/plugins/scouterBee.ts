bc516515358ad21340774aa010354185028a6785
eirikhanasand <eirik.hanasand@gmail.com>
2026-05-24 14:46:27 +0200

    v1.4.11: fixed scout role issue

--- src/plugins/scouterBee.ts
+++ src/plugins/scouterBee.ts
@@ -1,11 +1,9 @@
 import type { FastifyInstance } from 'fastify'
 import { runProjectScout } from '#utils/scouterbee/projects.ts'
-import config from '#config'
 
 export default async function scouterBee(fastify: FastifyInstance) {
-    // Bun.cron('0 12 * * *', () => {
+    Bun.cron('0 12 * * *', () => {
         fastify.log.info('Starting scheduled npm vulnerability scout...')
         runProjectScout()
-        console.log(config.scout.role)
-    // })
+    })
 }
