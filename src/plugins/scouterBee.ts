222f7b23ffb152615eb42dcb6492dc0057168fb7
eirikhanasand <eirik.hanasand@gmail.com>
2026-05-24 14:43:27 +0200

    test

--- src/plugins/scouterBee.ts
+++ src/plugins/scouterBee.ts
@@ -2,8 +2,8 @@ import type { FastifyInstance } from 'fastify'
 import { runProjectScout } from '#utils/scouterbee/projects.ts'
 
 export default async function scouterBee(fastify: FastifyInstance) {
-    Bun.cron('0 12 * * *', () => {
+    // Bun.cron('0 12 * * *', () => {
         fastify.log.info('Starting scheduled npm vulnerability scout...')
         runProjectScout()
-    })
+    // })
 }
